a28c097a820e557fd049d814ecb945e2b8e43809
Animesh Sharma <animeshsharma97@gmail.com>
2018-05-17 01:25:35 +0530

    Added user login

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,35 @@
 
 from flask_wtf import FlaskForm
-from wtforms import TextAreaField , SubmitField
-from wtforms.validators import DataRequired
+from wtforms import TextAreaField , StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from app.models import User
 
-class LoginForm(FlaskForm):
+
+class SubmitForm(FlaskForm):
     review = TextAreaField('Your review goes here : ', validators=[DataRequired()])
     submit = SubmitField('Predict rating')
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,59 @@
 
 from flask import render_template, flash, redirect , request, url_for
-from app import app
+from app import app , db
 import numpy as np
-from app.forms import LoginForm
+from flask_login import current_user, login_user , logout_user , login_required
+from app.models import User
+from app.forms import LoginForm , SubmitForm , RegistrationForm
 from app.output import *
+from werkzeug.urls import url_parse
 
-@app.route('/',methods=['GET', 'POST'])
+@app.route('/')
+@app.route('/index')
 def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('submit'))
     form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+
+        return redirect(url_for('submit'))
+    return render_template('login.html', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('submit'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/submit', methods=['GET', 'POST'])
+@login_required
+def submit():
+    form = SubmitForm()
     if form.validate_on_submit():
         text = form.review.data
         matrix = tokenizer.texts_to_matrix([text])
@@ -22,4 +68,4 @@ def index():
             return render_template('star.html' , user_image = "https://dublinbookworm.files.wordpress.com/2015/07/4-star2.jpg?w=300&h=60")
         else:
             return render_template('star.html' , user_image = "https://dublinbookworm.files.wordpress.com/2015/07/5-star2.jpg?w=300&h=60")
-    return render_template('index.html', form=form)
+    return render_template('submit.html', form=form)
